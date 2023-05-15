725b9dbbf1bad0767f8d8351e17bbf676d0ecdf2
Sonia-Baibou <s.baibou@ohmycode.io>
2023-05-15 21:18:09 +0200

    add input + submit btn

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  public inputValue!: string;
 
   constructor() {}
 
+  public onSubmit() {
+    console.log(this.inputValue);
+    // clean input
+    if (!this.inputValue) {
+      console.log('this field needs something !');
+    } else {
+      const inputWord = this.inputValue.trim();
+      if (inputWord.length <= 1) {
+        console.log('this field needs something !');
+      } else {
+        console.log(inputWord.length);
+        // faire appel au service
+      }
+    }
+  }
 }
