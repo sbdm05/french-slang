0aa31e669d067f6dc5e26418a233934acbf94e55
Sonia-Baibou <s.baibou@ohmycode.io>
2023-05-15 13:53:04 +0200

    remove saved words

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -19,12 +19,53 @@ export class Tab3Page {
     if (localStorage.getItem('saved')) {
       // extract localStorage
       this.tabAlreadySaved = JSON.parse(localStorage.getItem('saved') ?? '[]');
-      console.log(this.tabAlreadySaved)
-
+      console.log(this.tabAlreadySaved);
     } else {
       // save new localStorage
       console.log('pas de storage');
+    }
+  }
 
+  public onDelete(i: Word) {
+    if (localStorage.getItem('saved')) {
+      // extract localStorage
+      this.tabAlreadySaved = JSON.parse(localStorage.getItem('saved') ?? '[]');
+      console.log(this.tabAlreadySaved);
+      this.tabAlreadySaved = this.tabAlreadySaved.filter(
+        (item) => item._id !== i._id
+      );
+      localStorage.setItem('saved', JSON.stringify(this.tabAlreadySaved));
+    } else {
+      // save new localStorage
+      console.log('pas de storage');
     }
   }
 }
+
+//  const findItem = local.find((item) => item._id === obj._id);
+//       if (!findItem) {
+//         obj.isLiked = true;
+//         this.tabAlreadyLiked.unshift(obj);
+//         localStorage.setItem('liked', JSON.stringify(this.tabAlreadyLiked));
+//         this.onIncrement(obj);
+//       }
+//       if (findItem) {
+//         // decrement
+//         console.log('to decrement');
+//         obj.isLiked = false;
+//         this.tabAlreadyLiked = this.tabAlreadyLiked.filter(
+//           (item) => item._id !== findItem._id
+//         );
+//         // decrement dans mongodb
+//         // update localstorage
+//         localStorage.setItem('liked', JSON.stringify(this.tabAlreadyLiked));
+//         this.onDecrement(obj);
+//       }
+//     } else {
+//       // save new localStorage
+//       console.log(obj);
+//       obj.isLiked = true;
+//       this.tabAlreadyLiked.unshift(obj);
+//       localStorage.setItem('liked', JSON.stringify(this.tabAlreadyLiked));
+//       this.onIncrement(obj);
+//     }
